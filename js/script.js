38e9d17868f20cfd4ee44fb844ebb64a43d5926e
Audumn Vernon <audumnv@gmail.com>
2021-04-17 21:03:34 -0400

    update script

--- js/script.js
+++ js/script.js
@@ -3,6 +3,11 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  prompt ("How many characters do you want in your password?")
+  confirm("Click OK to confirm including special characters.")
+  confirm("Click OK to confirm including numeric characters.")
+  confirm("Click OK to confirm including lowercase characters.")
+  confirm("Click OK to confirm including uppercase characters.")
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -12,4 +17,14 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword() {
+  var length = 8,
+        charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+        retVal = "";
+    for (var i = 0, n = charset.length; i < length; ++i) {
+        retVal += charset.charAt(Math.floor(Math.random() * n));
+    }
+    return retVal;
+}
 
