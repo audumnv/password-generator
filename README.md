c4c2550f6f22e826dc9b86f678b1e5a3be87606f
Audumn Vernon <audumnv@gmail.com>
2021-04-17 22:47:05 -0400

    update

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# password-generator
- 
+# Password Generator
+
+## Description
+
+This project is a password generator, that returns a password at random based on selected criteria.
--- js/script.js
+++ js/script.js
@@ -3,28 +3,59 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  prompt ("How many characters do you want in your password?")
-  confirm("Click OK to confirm including special characters.")
-  confirm("Click OK to confirm including numeric characters.")
-  confirm("Click OK to confirm including lowercase characters.")
-  confirm("Click OK to confirm including uppercase characters.")
+  var length = Number(prompt("How many characters would you like?"));
+while (isNaN(length) || length < 8 || length > 128);
+var uppercase = confirm("Would you like uppercase letters?");
+var lowercase = confirm("Would you like lowercase letters?");
+var numbers = confirm("Would you like to use numbers?");
+var symbols = confirm("Would you like to use special characters?");
+ while (!uppercase && !lowercase && !numbers && !symbols) {
+  uppercase = confirm("Would you like to use uppercase letters?");
+  lowercase = confirm("Would you like to use lowercase letters?");
+  numbers = confirm("Would you like numbers?");
+  symbols = confirm("Would you like special characters?");
+}
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+
+
+
 
   passwordText.value = password;
 
 }
 
 // Add event listener to generate button
+
+
+
 generateBtn.addEventListener("click", writePassword);
 
+
+
 function generatePassword() {
-  var length = 8,
-        charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
-        retVal = "";
-    for (var i = 0, n = charset.length; i < length; ++i) {
-        retVal += charset.charAt(Math.floor(Math.random() * n));
-    }
-    return retVal;
+  var charset = "";
+  ranPassword = "";
+  if (uppercase) ranPassword += random(charset.uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+  if (lowercase) ranPassword += random(charset.lowercase = "abcdefghijklmnopqrstuvwxyz");
+  if (numbers) ranPassword += random(charset.numbers = "1234567890");
+  if (symbols) ranPassword += random(charset.symbols = "!@#$%^&*(){}[]=<>/,.");
+
+  for (var i = ranPassword.length; i < length; i++) ranPassword += random(random(charset).value);
+
+return ranPassword;
+
 }
 
+
+
+
+
+
+
+
+
+   
+
+
+  
